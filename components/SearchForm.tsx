19633c58717a3b882bb755a5ca0448c464bf67d6
yukito <iyggf66974@yahoo.ne.jp>
2025-02-06 09:08:35 +0900

    err

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Input } from './ui/input'
 import {
@@ -8,14 +10,42 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { Button } from './ui/button'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 
 
 const SearchForm = () => {
+  const searchParams = useSearchParams()
+  const search = searchParams.get('search') || '';
+  const jobStatus = searchParams.get('jobStatus') || 'all';
+
+  const router = useRouter()
+  const pathNama = usePathname();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    let params = new URLSearchParams()
+
+
+    const formData = new FormData(e.currentTarget)
+    const search = formData.get('search') as string
+    const jobStatus = formData.get('jobStatus') as string
+    params.set('search', search)
+    params.set('jobStatus', jobStatus)
+
+    router.push(`${pathNama}?${params.toString()}`)
+  }
+
+
+
+
+
+
+
+
   return (
     <div>
-      <form action="">
-        <Input type="text" placeholder="Search..."  name='search' />
+      <form action="" onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Search..."  name='search' defaultValue={search} />
 
         <Select name='jobStatus'>
           <SelectTrigger className="w-[180px]">
